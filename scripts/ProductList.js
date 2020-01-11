7ce32e2e1b81094f259666d3989b40efb20435a0
ETamez9173 <elitamez17@gmail.com>
2020-01-11 13:03:38 -0600

    first commit

---
+++ scripts/ProductList.js
@@ -0,0 +1,32 @@
+import { useProducts } from "./ProductProvider.js"
+import { useProductTypes } from "./ProductTypeProvider.js"
+import Product from "./Product.js"
+
+const contentTarget = document.querySelector(".products")
+
+export const ProductList = () => {
+const products = useProducts()
+const productTypes = useProductTypes()
+
+const render = () => {
+
+contentTarget.innerHTML = products.map(product => {
+
+// Find this product type
+const type = productTypes.find(type => type.id === product.productTypeId)
+
+
+// Get HTML rep of product
+
+const html = Product(product, type)
+
+return html
+
+}).join("")
+
+
+}
+render()
+}
+
+export default ProductList
